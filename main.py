f20b608c577c269163e1b9d9f2c653ef4cfdc541
Simon Muia <simonmuia38@gmail.com>
2023-01-23 15:14:07 +0000

    print value of the popped card

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ suits = ["spades","clubs","hearts","diamonds"]
 
 #store ranks in a dictionary
 ranks = [
-        
+      {"rank":"A", "value":11},
+      {"rank":"2", "value":2},
+      {"rank":"3", "value":3},
+      {"rank":"5", "value":5},
+      {"rank":"6", "value":6},
+      {"rank":"7", "value":7},
+      {"rank":"8", "value":8},
+      {"rank":"9", "value":9},
+      {"rank":"10", "value":10},
+      {"rank":"J", "value":10},
+      {"rank":"Q", "value":10},
+      {"rank":"K", "value":10}     
           ]
 
 #print each suit
 for suit in suits:
   for rank in ranks:
-    cards.append([suit, rank[0]]) #add cthe 52 cards to the list
+     #add the 52 cards to the list
+    cards.append([suit, rank])
 
 
 #Shuffle cards
@@ -32,5 +44,7 @@ shuffle()
 #store both rank name and value in a linked list
 #create first card and store it outside card deck
 card = deal(1)[0]
+
+print(card[1]["value"])
 
 
