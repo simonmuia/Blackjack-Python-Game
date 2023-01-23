958317546ea67e222b6087ebfe4a3123b1af2820
Simon Muia <simonmuia38@gmail.com>
2023-01-23 15:18:17 +0000

    add init method

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import random
-#create a deck class
 
+#create a deck class
 class Deck:
-  #Define list of card names
-  cards = []
-  suits = ["spades","clubs","hearts","diamonds"]
-  
-  #store ranks in a dictionary
-  ranks = [
-        {"rank":"A", "value":11},
-        {"rank":"2", "value":2},
-        {"rank":"3", "value":3},
-        {"rank":"5", "value":5},
-        {"rank":"6", "value":6},
-        {"rank":"7", "value":7},
-        {"rank":"8", "value":8},
-        {"rank":"9", "value":9},
-        {"rank":"10", "value":10},
-        {"rank":"J", "value":10},
-        {"rank":"Q", "value":10},
-        {"rank":"K", "value":10}     
-            ]
-  
-  #print each suit
-  for suit in suits:
-    for rank in ranks:
-       #add the 52 cards to the list
-      cards.append([suit, rank])
+  #create initial function
+  def __init__(self):
+    #Define list of card names
+    cards = []
+    suits = ["spades","clubs","hearts","diamonds"]
+    
+    #store ranks in a dictionary
+    ranks = [
+          {"rank":"A", "value":11},
+          {"rank":"2", "value":2},
+          {"rank":"3", "value":3},
+          {"rank":"5", "value":5},
+          {"rank":"6", "value":6},
+          {"rank":"7", "value":7},
+          {"rank":"8", "value":8},
+          {"rank":"9", "value":9},
+          {"rank":"10", "value":10},
+          {"rank":"J", "value":10},
+          {"rank":"Q", "value":10},
+          {"rank":"K", "value":10}     
+              ]
+    
+    #print each suit
+    for suit in suits:
+      for rank in ranks:
+         #add the 52 cards to the list
+        cards.append([suit, rank])
   
   
   #Shuffle cards
